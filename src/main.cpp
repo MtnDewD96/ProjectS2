9618a8b47606fa37831817ddc19fd2e2ae80f3a3
florentcournoyer16 <florentcournoyer16@gmail.com>
2021-02-14 14:04:37 -0500

    commit florent no 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
-#include <stdio.h>
+// command_line_arguments.cpp
+// compile with: /EHsc
+#include <iostream>
 
 using namespace std;
-
-int main()
+int main( int argc,      // Number of strings in array argv
+          char *argv[],   // Array of command-line argument strings
+          char *envp[] )  // Array of environment variable strings
 {
-    printf("hello world");
-    return 0;
+    int count;
+
+    // Display each command-line argument.
+    cout << "allo";
 }
